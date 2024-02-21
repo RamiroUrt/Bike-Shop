2fd2408c8695f3f2555cd18f00b60c4268289a6e
RamiroUrt <121266524+RamiroUrt@users.noreply.github.com>
2024-02-21 13:34:31 -0300

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -36,7 +36,6 @@ export const Hero = () => {
         </div>
       </div>
       <div className="md:col-span-3 flex items-center mt-4 xl:mt-0">
-        <div className="">
             <img src="../../logo.jpg" alt="" className='mt-5 xl:mt-0' data-aos="fade-right"/>
             <div className="relative bg-white shadow-xl rounded-lg p-4 flex flex-col justify-center gap-2 max-w-[250px] mx-auto -mt-12">
             <div className="flex items-center">
@@ -67,7 +66,6 @@ export const Hero = () => {
             <div className="flex items-center gap-2 text-lg text-gray-500">
               <RiStarFill className="text-primary" /> 5.0 (3.1k Reviews)
             </div>
-        </div>
       </div>
     </div>
   </div>
